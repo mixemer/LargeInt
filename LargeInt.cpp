4a4498c39af47f4d30eddb954a83ae3d5729823c
mixemer <mehmetshin96@gmail.com>
2018-09-22 21:11:58 -0400

    Adds LargeInt.cpp; Adds LargeInt.hpp
    A LargeInt class that use doubly linkedlist to store large int values

---
+++ LargeInt.cpp
@@ -0,0 +1,138 @@
+//
+//  LargeInt.cpp
+//  csc311Project1
+//
+//  Created by Mehmet Sahin on 9/14/18.
+//  Copyright © 2018 Mehmet Sahin. All rights reserved.
+//
+
+#include "LargeInt.hpp"
+
+LargeInt:: LargeInt(const std::string& num)
+{
+    fillTheList(num);
+}
+
+LargeInt:: LargeInt(const UDList<int> &l)
+{
+    list = l;
+}
+// could override what is inside
+void LargeInt:: operator= (const LargeInt& other)
+{
+    UDList<int>::iterator head = list.begin();
+    if (head){
+        list.makeEmpty();
+    }
+    list = other.list;
+}
+// could override what is inside
+void LargeInt:: operator= (const std::string& num)
+{
+    UDList<int>::iterator head = list.begin();
+    if (head){
+        list.makeEmpty();
+    }
+    fillTheList(num);
+}
+// could override what is inside
+void LargeInt:: fillTheList(const std::string& num)
+{
+    long size = num.size();
+    if (size > 0){
+        for (int i = 0; i < size; i++){
+            if (!isdigit(num[i])){
+                std::cout << string("Bad data type");
+                return;
+            }
+            list.insertBack(num[i] - '0');
+        }
+    }
+}
+
+LargeInt LargeInt:: operator+ (const LargeInt& other)
+{
+    UDList<int> temp;
+    bool carry = false;
+    UDList<int>::iterator ithis = list.end();
+    UDList<int>::iterator iother = other.list.end();
+    
+    while (ithis || iother)
+    {
+        int total = 0;
+        if (carry) total += 1;
+        if (ithis && iother){
+            total += (ithis->info + iother->info);
+            ithis = ithis->prev;
+            iother = iother->prev;
+        }else if (ithis){
+            total += ithis->info;
+            ithis = ithis->prev;
+        }else{
+            total += iother->info;
+            iother = iother->prev;
+        }
+        
+        if (total > 9){
+            carry = true;
+            temp.insertFront(total % 10);
+        }else{
+            carry = false;
+            temp.insertFront(total);
+        }
+    }
+    
+    if (carry) temp.insertFront(1);
+    return LargeInt(temp);
+}
+
+bool LargeInt:: operator== (const LargeInt& other)
+{
+    UDList<int>::iterator ithis = list.begin();
+    UDList<int>::iterator iother = other.list.begin();
+    
+    if (list.getLength() != other.list.getLength())
+        return false;
+    
+    while (ithis && iother) {
+        if (ithis->info != iother->info)
+            return false;
+        ithis = ithis->next;
+        iother = iother->next;
+    }
+    
+    return true;
+}
+
+std::ostream& operator<< (std::ostream& out, const LargeInt& largeInt)
+{
+    UDList<int>::iterator it = largeInt.list.begin();
+    
+    while (it) {
+        out << it->info;
+        it = it->next;
+    }
+    return out;
+}
+
+// could override what is inside ???? check with professor
+std::istream& operator>> (std::istream& in,LargeInt& largeInt)
+{
+    std::string num;
+    in >> num;
+    if (largeInt.list.getLength() == 0)
+        largeInt = num;
+    return in;
+}
+
+
+
+void LargeInt:: print() const
+{
+    list.printFromFront();
+}
+
+
+
+
+
---
+++ LargeInt.hpp
@@ -0,0 +1,100 @@
+//
+//  LargeInt.hpp
+//  csc311Project1
+//
+//  Created by Mehmet Sahin on 9/14/18.
+//  Copyright © 2018 Mehmet Sahin. All rights reserved.
+//
+
+#ifndef LargeInt_hpp
+#define LargeInt_hpp
+
+#include <iostream>
+#include "UDList.hpp"
+#include "UDList.cpp"
+using namespace std;
+
+// TODO: ask prof if it is okey to use (=) to insert or should it stop if overrides
+
+class LargeInt{
+private:
+    UDList<int> list; // an empty doubly linked list
+    
+    /**
+     * fills the list with given string num by parting num into individual digits.
+     * will be used for constructor and assignment oporator
+     * @param num is an int
+     */
+    void fillTheList(const std::string& num);
+public:
+    LargeInt() {};
+    
+    /**
+     * Overloaded constructor so it can get an string num so it can fill the nodes of the list with digits of the num
+     * @param num is an int
+     */
+    LargeInt(const std::string& num);
+    
+    /**
+     * copy constructor to copy other object's data into this object.
+     * @param other is a reference to UDList<x> object
+     */
+    LargeInt(const UDList<int>& other);
+    
+    /**
+     * Overloaded assignment operator to copy other object's data into this object.
+     * @param other is a reference to LargeInt object
+     */
+    void operator= (const LargeInt& other);
+    
+    /**
+     * Overloaded assignment operator fills the nodes of the list with digits of the num
+     * @param num is an int
+     */
+    void operator= (const std::string& num);
+    
+    /**
+     * Adds all the number digits of the both this and otehr LargeInt objects.
+     * @param other is a reference to LargeInt object
+     * @return LargeInt that was added by this and other LargeInt
+     */
+    LargeInt operator+ (const LargeInt& other);
+    
+    /**
+     * Overloaded bool operator to check if this and other LargeInt objects contain the same data
+     * @param other is a reference to LargeInt object
+     */
+    bool operator== (const LargeInt& other);
+    
+    /**
+     * Overloaded output operator to output data of this object to files
+     * @param out is a reference to istream object
+     * @largeInt is areference to LargeInt object
+     */
+    friend std::ostream& operator<< (std::ostream& out, const LargeInt& largeInt);
+    
+    /**
+     * Overloaded input operator to get data from and file and store it in this LargeInt object.
+     * @param in is a reference to istream object
+     * @largeInt is areference to LargeInt object
+     */
+    friend std::istream& operator>> (std::istream& in, LargeInt& largeInt);
+    
+    /**
+     * Prints the information of nodes of this list
+     */
+    void print() const;
+};
+
+#endif /* LargeInt_hpp */
+
+
+
+
+
+
+
+
+
+
+
